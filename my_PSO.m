b837335d2546225176f3739b7ffdd22644e43f3a
FranciscoGlz <31779321+FranciscoGlz@users.noreply.github.com>
2017-09-26 17:38:54 -0500

    my_PSO.m
    Al compilar el código de mi compañero, sentí que tardaba un poco más en procesarlo, con este código tengo más velocidad para llegar a nuestro resultado.

--- my_PSO.m
+++ my_PSO.m
@@ -1,74 +1,77 @@
 function my_PSO(fun,fronteras)
 
-%%%%%%%%%%%%%%%%%Preámbulo%%%%%%%%%%%%%%
-D = size(fronteras,1);%Dimensiones
-M = 100;%Iteraciones
-N = 30;%Partículas
+%Preambulo
+D=size(fronteras,1);
+M=100;
+N=30;
+V=zeros(N,D);
+C1=2.5;
+C2=2.5;
+W=0.7;
 
-C1 = 2.5;%coef. Conf. Enjambre
-C2 = 2.5;%coef. Conf. Propia
-W  = 0.7;%Coef. Inercia
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-V  = zeros(N,D);
-
-%%%%%%%%%%%%%Inicialización%%%%%%%%%%%%%%%%%%%%
-Aleatorios = rand(N,D);
-
-P  = nan(size(Aleatorios));
-
-for n = 1:N
-    P(n,:) = fronteras(:,1)' + (fronteras(:,2)-fronteras(:,1))' .* Aleatorios(n,:);
+%Inicializacion
+Aleatorios=rand(N,D);
+P=nan(size(Aleatorios));
+for n=1:N
+    P(n,:)=fronteras(:,1)' + (fronteras(:,2)-fronteras(:,1))'.*Aleatorios(n,:);
 end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%plot(P(:,1),P(:,2),'*')%Prueba
 
-%%%%%%%%%%%%%%%Evaluación de f%%%%%%%%%%%%%%%%%%%
-F = nan(N,1); % Función evaluada
+%plot(P(:,1),P(:,2),'o')
+%>>my_PSO(o,[-10,10;-5,5])
 
-for n = 1:N
-    F(n) = fun(P(n,:));
+%Evaluacion de funcion
+F=nan(N,1);
+for n=1:N
+    F(n)=fun(P(n,:));
 end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%[X,Y] = meshgrid(linspace(fronteras(1,1),fronteras(1,2),50),linspace(fronteras(2,1),fronteras(2,2),50));%Prueba
-%surf(X,Y,reshape(fun([X(:),Y(:)]),50,50));shading interp; hold on; colormap winter;
-%plot3(P(:,1),P(:,2),F,'ro','MarkerFaceColor','y');
 
-%%%%%%%%%%%%%%%%%%Identificar%%%%%%%%%%%%5
-Pgi = P;%Son iguales
-[Fbest,g] = min(F);
-Pg = P(g,:);
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Prueba 2D
+[X,Y]= meshgrid(linspace(fronteras(1,1),fronteras(1,2),50),linspace(fronteras(2,1),fronteras(2,2),50));
+surf(X,Y,reshape(fun([X(:),Y(:)]),50,50));
+shading interp; hold on; colormap winter;
+plot3(P(:,1),P(:,2),F,'ro','MarkerFaceColor','y');
 
-%plot3(Pg(1),Pg(2),Fbest,'Or','MarkerFaceColor','r','MarkerSize',10);
+%>>my_PSO(@(X) X(:,1).^2 + X(:,2).^2, [-5 5, -10 10]);
 
-%%%%%%%%%%%%%%%%%%%%%%%calculo del movimiento%%%%%%%%%%%%%%%%5
-for m = 1:M
-    for n = 1:N
-        V(n,:) = W*(V(n,:) + C1*rand(1,D) .* (Pg-P(n,:))+...
-            C2*rand(1,D) .* (Pgi(n,:)-P(n,:)));
-        P(n,:) = P(n,:) + V(n,:);
+Pgi=P; %Son iguales comenzando 
+[Fbest,g]=min(F);
+Pg=P(g,:);
+plot3(Pg(1),Pg(2),Fbest,'or','MarkerFaceColor','r','Markersize',10);
+hold on; getframe(gcf);
+
+%Pgi = El historial de la mejor posicion de la particula
+
+%Calculo de P^(t+1)
+for m=1:M
+    for n=1:N
+        V(n,:)=W*(V(n,:)+C1*rand(1,D).*(Pg-P(n,:))+...
+            C2*rand(1,D).*(Pgi(n,:)-P(n,:)));
+        P(n,:)=P(n,:)+V(n,:);
     end
-   
-    F(n) = fun(P(n,:));
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%%%%%%%%%%%%%%%%%%%Identificar Pgi%%%%%%%%%%%%%%
-    [Fbest,g] = min(F);
-    Pg = P(g,:);
+    F=nan(N,1);
+for n=1:N
+    F(n)=fun(P(n,:));
+end
+
+%Identificar Pg
+[Fbest,g]=min(F);
+Pg=P(g,:);
+
+%Identificar Pgi
+for n=1:N
+    if fun(Pgi(n,:)) > F(n)
+        Pgi(n,:)=P(n,:);
+    end
+end
+
+[X,Y]= meshgrid(linspace(fronteras(1,1),fronteras(1,2),50),linspace(fronteras(2,1),fronteras(2,2),50));
+surf(X,Y,reshape(fun([X(:),Y(:)]),50,50));
+shading interp; hold on; colormap winter;
+plot3(P(:,1),P(:,2),F,'ro','MarkerFaceColor','y');
+plot3(Pg(1),Pg(2),Fbest,'or','MarkerFaceColor','r','Markersize',10);
+hold off; 
+axis([fronteras(1,:),fronteras(2,:)]);
+getframe(gcf);
     
-    for n = 1:N
-        if fun(Pgi(n,:) > F(n))
-            Pgi(n,:) = P(n,:);
-        end
-    end
-    [X,Y] = meshgrid(linspace(fronteras(1,1),fronteras(1,2),50),linspace(fronteras(2,1),fronteras(2,2),50));%Prueba
-    surf(X,Y,reshape(fun([X(:),Y(:)]),50,50));shading interp; hold on; colormap winter;
-    plot3(P(:,1),P(:,2),F,'ro','MarkerFaceColor','y');
-    plot3(Pg(1),Pg(2),Fbest,'Or','MarkerFaceColor','r','MarkerSize',10);
-    axis([fronteras(1,:),fronteras(2,:)]);title(sprintf('ite=%d, Fbest = %.4g',m,Fbest));
-    hold off;view(2);getframe(gcf);pause(0.05);
-end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-end
+end;
